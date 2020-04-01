dad76258c2290c2162c0f1bc735a532a875b0b91
Abraão Rossetto <abraaomirandar@gmail.com>
2020-04-01 12:07:15 -0300

    Make move for won and lost game

--- test/hangman/game_test.exs
+++ test/hangman/game_test.exs
@@ -3,11 +3,16 @@ defmodule Hangman.GameTest do
 
   alias Hangman.Game
 
-  test "new_game returns structure" do
+  test "new_game/2 returns structure" do
     game = Game.new_game()
 
     assert game.turns_left == 7
     assert game.game_state == :initializing
     assert length(game.letters) > 0
+  end
+
+  test "make_move/2 do not change game_state if the game is won or lost" do
+    game = Game.new_game() |> Map.put(:game_state, :lost)
+    {^game, _} = Game.make_move(game, "wtv")
   end
 end
